cc857daf7478dcc40f45873dfc90206be0099766
IRT <dev@figaf.com>
2024-10-31 13:33:40 +1100

    The following objects were updated: IntegrationFlow

---
+++ FIGAFTESTPACKAGE/IntegrationFlow/HTTP_ACK_To_Common_ACK_copy/src/main/resources/script/script1.groovy
@@ -0,0 +1,20 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import groovy.xml.*;
+
+
+def Message processData(Message message) {
+    //Body
+    def body = message.getBody(String) as String;
+    
+    def root = new XmlParser().parseText(body);
+    
+    def ackFor = root.AcknowledgementFor.text()
+    if (ackFor.startsWith("Order")) {
+        root.'**'.find { it.name() == 'AcknowledgementFor' }.value = ackFor.toUpperCase()
+    }
+    
+    def xml = XmlUtil.serialize(root)
+    message.setBody(xml)
+    return message;
+}
