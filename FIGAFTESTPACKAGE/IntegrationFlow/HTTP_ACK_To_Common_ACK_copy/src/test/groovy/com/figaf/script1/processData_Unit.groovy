4bbd7be7fc951eecace2cea3455c405bc4b0e5f3
IRT <dev@figaf.com>
2024-11-05 23:35:11 +1100

    The following objects were updated: IntegrationFlow

--- FIGAFTESTPACKAGE/IntegrationFlow/HTTP_ACK_To_Common_ACK_copy/src/test/groovy/com/figaf/script1/processData_Unit.groovy
+++ FIGAFTESTPACKAGE/IntegrationFlow/HTTP_ACK_To_Common_ACK_copy/src/test/groovy/com/figaf/script1/processData_Unit.groovy
@@ -2,6 +2,7 @@ package com.figaf.script1
 
 import com.figaf.MessageImpl
 import com.figaf.utils.XMLUtils
+import com.sap.gateway.ip.core.customdev.util.Message
 import com.sap.it.api.msglog.MessageLog
 import com.sap.it.api.msglog.MessageLogFactory
 import groovy.json.JsonSlurper
@@ -26,97 +27,135 @@ class processData_Unit extends Specification {
     @Shared GroovyShell shell = new GroovyShell()
     @Shared Script script
 
-    static jsonSlurper = null
+    private Message msg
 
-    @Mock(lenient = true)
-    MessageLogFactory messageLogFactory
-
-    @Mock
-    MessageLog messageLog
-
-    MessageImpl message;
-
-    @BeforeEach
-    void beforeEach() {
-        given(messageLogFactory.getMessageLog(any()))
-                .willReturn(messageLog)
-        message = new MessageImpl()
-    }
-
-    @BeforeAll
-    static void setUp() {
-        shell = new GroovyShell()
-        jsonSlurper = new JsonSlurper()
-
-        Binding binding = new Binding()
-        GroovyShell shell = new GroovyShell(binding)
+    def setupSpec() {
+        // Load Groovy Script
         script = shell.parse(new File("src/main/resources/script/script1.groovy"))
     }
 
-    @Test
-    void testProcessDataWithSampleBodyOfOrders() {
-        def body = '''<?xml version="1.0" encoding="UTF-8"?>
-<AcknowledgementMetadata>
-    <AcknowledgementFor>Orders</AcknowledgementFor>
-    <AcknowledgementForFileName>Order_1.xml</AcknowledgementForFileName>
-    <SentDateTime>22-03-2024T22:32:22</SentDateTime>
-    <Receipt expected="true">
-        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
-        <Received>false</Received>
-        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
-        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
-    </Receipt>
-    <Processed expected="true">
-        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
-        <Received>false</Received>
-        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
-        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
-    </Processed>
-    <Technical>
-        <NoReceiptAckProcessor>/cba/acknowledgements/noFileAck</NoReceiptAckProcessor>
-        <NoProcessedAckProcessor>/cba/acknowledgements/noFileAck</NoProcessedAckProcessor>
-    </Technical>
-</AcknowledgementMetadata>'''
-        message.setBody(body)
-        MessageImpl actualMessage = script.processData(message);
-        actualMessage.getProperties().containsKey()
-        String actualValue = XMLUtils.getValueFromField((String)actualMessage.getBody(), "AcknowledgementFor")
-
-
-        assertEquals("ORDERS", actualValue)
+    def setup() {
+        this.msg = new MessageImpl()
     }
 
-    @Test
-    void testProcessDataWithSampleBodyOfPayments() {
+    def "Acknowledgement of Orders has a uppercase for AcknowledgementForTag"() {
         def body = '''<?xml version="1.0" encoding="UTF-8"?>
-<AcknowledgementMetadata>
-    <AcknowledgementFor>Payments</AcknowledgementFor>
-    <AcknowledgementForFileName>Order_1.xml</AcknowledgementForFileName>
-    <SentDateTime>22-03-2024T22:32:22</SentDateTime>
-    <Receipt expected="true">
-        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
-        <Received>false</Received>
-        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
-        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
-    </Receipt>
-    <Processed expected="true">
-        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
-        <Received>false</Received>
-        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
-        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
-    </Processed>
-    <Technical>
-        <NoReceiptAckProcessor>/cba/acknowledgements/noFileAck</NoReceiptAckProcessor>
-        <NoProcessedAckProcessor>/cba/acknowledgements/noFileAck</NoProcessedAckProcessor>
-    </Technical>
-</AcknowledgementMetadata>'''
-        message.setBody(body)
-        MessageImpl actualMessage = script.processData(message);
-
-        String actualValue = XMLUtils.getValueFromField((String)actualMessage.getBody(), "AcknowledgementFor")
-
-
-        assertEquals("Payments", actualValue)
+            <AcknowledgementMetadata>
+                <AcknowledgementFor>Orders</AcknowledgementFor>
+                <AcknowledgementForFileName>Order_1.xml</AcknowledgementForFileName>
+                <SentDateTime>22-03-2024T22:32:22</SentDateTime>
+                <Receipt expected="true">
+                    <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+                    <Received>false</Received>
+                    <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+                    <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+                </Receipt>
+                <Processed expected="true">
+                    <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+                    <Received>false</Received>
+                    <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+                    <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+                </Processed>
+                <Technical>
+                    <NoReceiptAckProcessor>/cba/acknowledgements/noFileAck</NoReceiptAckProcessor>
+                    <NoProcessedAckProcessor>/cba/acknowledgements/noFileAck</NoProcessedAckProcessor>
+                </Technical>
+            </AcknowledgementMetadata>'''
+        
     }
+
+
+//    static jsonSlurper = null
+//
+//    @Mock(lenient = true)
+//    MessageLogFactory messageLogFactory
+//
+//    @Mock
+//    MessageLog messageLog
+//
+//    MessageImpl message;
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        given(messageLogFactory.getMessageLog(any()))
+//                .willReturn(messageLog)
+//        message = new MessageImpl()
+//    }
+//
+//    @BeforeAll
+//    static void setUp() {
+//        shell = new GroovyShell()
+//        jsonSlurper = new JsonSlurper()
+//
+//        Binding binding = new Binding()
+//        GroovyShell shell = new GroovyShell(binding)
+//        script = shell.parse(new File("src/main/resources/script/script1.groovy"))
+//    }
+//
+//    @Test
+//    void testProcessDataWithSampleBodyOfOrders() {
+//        def body = '''<?xml version="1.0" encoding="UTF-8"?>
+//<AcknowledgementMetadata>
+//    <AcknowledgementFor>Orders</AcknowledgementFor>
+//    <AcknowledgementForFileName>Order_1.xml</AcknowledgementForFileName>
+//    <SentDateTime>22-03-2024T22:32:22</SentDateTime>
+//    <Receipt expected="true">
+//        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+//        <Received>false</Received>
+//        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+//        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+//    </Receipt>
+//    <Processed expected="true">
+//        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+//        <Received>false</Received>
+//        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+//        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+//    </Processed>
+//    <Technical>
+//        <NoReceiptAckProcessor>/cba/acknowledgements/noFileAck</NoReceiptAckProcessor>
+//        <NoProcessedAckProcessor>/cba/acknowledgements/noFileAck</NoProcessedAckProcessor>
+//    </Technical>
+//</AcknowledgementMetadata>'''
+//        message.setBody(body)
+//        MessageImpl actualMessage = script.processData(message);
+//        actualMessage.getProperties().containsKey()
+//        String actualValue = XMLUtils.getValueFromField((String)actualMessage.getBody(), "AcknowledgementFor")
+//
+//
+//        assertEquals("ORDERS", actualValue)
+//    }
+//
+//    @Test
+//    void testProcessDataWithSampleBodyOfPayments() {
+//        def body = '''<?xml version="1.0" encoding="UTF-8"?>
+//<AcknowledgementMetadata>
+//    <AcknowledgementFor>Payments</AcknowledgementFor>
+//    <AcknowledgementForFileName>Order_1.xml</AcknowledgementForFileName>
+//    <SentDateTime>22-03-2024T22:32:22</SentDateTime>
+//    <Receipt expected="true">
+//        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+//        <Received>false</Received>
+//        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+//        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+//    </Receipt>
+//    <Processed expected="true">
+//        <ExpectedBy>22-03-2024T22:32:22</ExpectedBy>
+//        <Received>false</Received>
+//        <NoAcknowledementProcessCompleted>false</NoAcknowledementProcessCompleted>
+//        <NotificationRecipients>anuj.batra@tcs.com</NotificationRecipients>
+//    </Processed>
+//    <Technical>
+//        <NoReceiptAckProcessor>/cba/acknowledgements/noFileAck</NoReceiptAckProcessor>
+//        <NoProcessedAckProcessor>/cba/acknowledgements/noFileAck</NoProcessedAckProcessor>
+//    </Technical>
+//</AcknowledgementMetadata>'''
+//        message.setBody(body)
+//        MessageImpl actualMessage = script.processData(message);
+//
+//        String actualValue = XMLUtils.getValueFromField((String)actualMessage.getBody(), "AcknowledgementFor")
+//
+//
+//        assertEquals("Payments", actualValue)
+//    }
 
 }
